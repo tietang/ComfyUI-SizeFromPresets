0566c660428fff0b852b6d51b28a224dcc3403d4
Tietang Wang <30461739@qq.com>
2024-06-07 22:26:17 +0800

    Update node.py

--- nodes/node.py
+++ nodes/node.py
@@ -14,9 +14,10 @@ def load_size_presets_input(file_name,digit):
     with open(os.path.join(presets_dir, file_name),'r') as f:
         reader = csv.reader(f)
         data = [row for row in reader]
-        size_presets = [[int(v.strip()) for v in row] for row in data[1:]]
+        # size_presets = [[int(v.strip()) for v in row] for row in data[1:]]
+        size_presets = [[v.strip() for v in row] for row in data[1:]]
 
-    return [f'{w: >{digit}} x {h: >{digit}}' for w,h in size_presets]
+    return [f'{r} - {w: >{digit}} x {h: >{digit}}' for w,h,r in size_presets]
     
 class SizeFromPresetsBase:
     def __init__(self):
@@ -31,21 +32,24 @@ class SizeFromPresetsBase:
         }
 
     RETURN_TYPES = ("INT","INT")
-    RETURN_NAMES = ("w","h")
+    RETURN_NAMES = ("width","height")
 
     FUNCTION = "get_size"
 
     CATEGORY = "SizeFromPresets"
 
     def get_size(self, preset):
-        parsed_preset = [int(v.strip()) for v in preset.split('x')]
+        # parsed_preset = [int(v.strip()) for v in preset.split('x')]
+        data = [ v for v in preset.split(' - ') ]
+        print(data)
+        parsed_preset = [int(v.strip()) for v in data[1].split('x')]
         return (parsed_preset[0], parsed_preset[1])
     
 class SizeFromPresetsSD15(SizeFromPresetsBase):
-    SIZE_PRESETS_INPUT = load_size_presets_input('sd15.csv',3)
+    SIZE_PRESETS_INPUT = load_size_presets_input('radio-sd15.csv',3)
     
 class SizeFromPresetsSDXL(SizeFromPresetsBase):
-    SIZE_PRESETS_INPUT = load_size_presets_input('sdxl.csv',4)
+    SIZE_PRESETS_INPUT = load_size_presets_input('radio-sdxl.csv',4)
 
 class EmptyLatentImageFromPresetsBase:
     def __init__(self):
@@ -66,12 +70,14 @@ class EmptyLatentImageFromPresetsBase:
     CATEGORY = "SizeFromPresets"
 
     def generate(self, preset, batch_size=1):
-        w,h = [int(v.strip()) for v in preset.split('x')]
+        # w,h = [int(v.strip()) for v in data[1].split('x')] 
+        data = [ v for v in preset.split(' - ') ]
+        w,h = [int(v.strip()) for v in data[1].split('x')] 
         latent = torch.zeros([batch_size, 4, h // 8, w // 8], device=self.device)
         return ({"samples":latent}, w, h)
     
 class EmptyLatentImageFromPresetsSD15(EmptyLatentImageFromPresetsBase):
-    SIZE_PRESETS_INPUT = load_size_presets_input('sd15.csv',3)
+    SIZE_PRESETS_INPUT = load_size_presets_input('radio-sd15.csv',3)
     
 class EmptyLatentImageFromPresetsSDXL(EmptyLatentImageFromPresetsBase):
-    SIZE_PRESETS_INPUT = load_size_presets_input('sdxl.csv',4)
+    SIZE_PRESETS_INPUT = load_size_presets_input('radio-sdxl.csv',4)
